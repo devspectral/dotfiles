05e2de8be4337f497c40f8eac046597b7f48d7dd
devspectral <devspectral@gmail.com>
2024-08-15 23:23:10 -0400

    Updated window, tab, and formatting

--- nvim/lua/spectral/core/options.lua
+++ nvim/lua/spectral/core/options.lua
@@ -11,7 +11,8 @@ opt.shiftwidth = 2 -- 2 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
-opt.wrap = false
+opt.wrap = true
+opt.textwidth = 85
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
