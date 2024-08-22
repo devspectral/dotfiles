2cca714f098b695f9d0f2561526da568dc300521
devspectral <devspectral@gmail.com>
2024-08-21 23:25:09 -0400

    updates...

--- nvim/lua/spectral/plugins/colorscheme.lua
+++ nvim/lua/spectral/plugins/colorscheme.lua
@@ -1,14 +1,12 @@
 return {
-  "olimorris/onedarkpro.nvim",
-  priority = 1000, -- Ensure it loads first
-  config = function()
-
-require("onedarkpro").setup({
-  colors = {
-    dark = { bg = "#181818" }, -- changed to darker background  
-  }
-})
-
-    vim.cmd("colorscheme onedark")
-  end
-}
+      'sainnhe/gruvbox-material',
+      lazy = false,
+      priority = 1000,
+      config = function()
+        -- Optionally configure and load the colorscheme
+        -- directly inside the plugin declaration.
+        vim.g.gruvbox_material_enable_italic = true
+        vim.g.gruvbox_material_background = 'hard'
+        vim.cmd.colorscheme('gruvbox-material')
+      end
+    }
