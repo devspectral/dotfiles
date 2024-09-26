6fe1b36dafab1b9077b083f4d72dc4bfa77e2372
devspectral <devspectral@gmail.com>
2024-09-26 12:18:40 -0400

    added new theme

--- nvim/lua/spectral/plugins/colorscheme.lua
+++ nvim/lua/spectral/plugins/colorscheme.lua
@@ -1,12 +1,50 @@
+-- return {
+-- 	"olimorris/onedarkpro.nvim",
+-- 	priority = 1000, -- Ensure it loads first
+-- 	config = function()
+-- 		require("onedarkpro").setup({
+-- 			colors = {
+-- 				dark = { bg = "#181818" },
+-- 			},
+-- 		})
+-- 		vim.cmd("colorscheme onedark")
+-- 	end,
+-- }
+
 return {
-	"olimorris/onedarkpro.nvim",
-	priority = 1000, -- Ensure it loads first
+	"rebelot/kanagawa.nvim",
+	priority = 1000,
 	config = function()
-		require("onedarkpro").setup({
-			colors = {
-				dark = { bg = "#181818" },
+		require("kanagawa").setup({
+			compile = false, -- enable compiling the colorscheme
+			undercurl = true, -- enable undercurls
+			commentStyle = { italic = true },
+			functionStyle = {},
+			keywordStyle = { italic = true },
+			statementStyle = { bold = true },
+			typeStyle = {},
+			transparent = false, -- do not set background color
+			dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+			terminalColors = true, -- define vim.g.terminal_color_{0,17}
+			colors = { -- add/modify theme and palette colors
+				palette = {
+                    sumiInk3 = "#181818",
+                    sumiInk4 = "#181818",
+                },
+				theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
 			},
+			-- overrides = function(colors) -- add/modify highlights
+			-- 	return {}
+			-- end,
+			theme = "wave", -- Load "wave" theme when 'background' option is not set
+			background = { -- map the value of 'background' option to a theme
+				dark = "wave", -- try "dragon" !
+				light = "lotus",
+			},
+
 		})
-		vim.cmd("colorscheme onedark")
+
+		-- setup must be called before loading
+		vim.cmd("colorscheme kanagawa-wave")
 	end,
 }
